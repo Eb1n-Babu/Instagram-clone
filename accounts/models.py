bea116759df284b7f4f7f089ef15d04af92296df
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-08-26 17:06:59 +0530

    model structure updated

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+from sqlalchemy.sql.functions import user
+
 
 # Create your models here.
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return user.username
