57dfec203a2cefec39379f97a0f32c1322c67936
Eb1n-Babu <ebin.babu.in@icloud.com>
2025-09-15 15:37:20 +0530

    create PostForm and CommentForm for feed interactions
    - PostForm handles image and caption input
    - CommentForm supports comment submission under each post
    - Both forms use ModelForm for clean integration

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Post, Comment
 from django.contrib.auth.forms import  UserCreationForm , AuthenticationForm
 
 
@@ -19,3 +20,14 @@ class LoginForm(AuthenticationForm):
     def confirm_login_allowed(self, user):
         if not user.is_active:
             raise forms.ValidationError("This user is inactive")
+
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['image', 'caption']
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['text']
